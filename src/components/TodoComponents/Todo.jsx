9bc01fd7ef9f5f0bedeeb41d6b84d86e6e2cb54c
Joel Bartlett <joel@murbar.com>
2019-02-13 17:18:24 -0500

    Pass item Id to handler

--- src/components/TodoComponents/Todo.jsx
+++ src/components/TodoComponents/Todo.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Todo = ({ item, toggleCompleted }) => {
   return (
-    <div className="todolist-todo" onClick={toggleCompleted}>
+    <div className="todolist-todo" onClick={() => toggleCompleted(item.id)}>
       {item.completed ? '[×]' : '[ ]'} - {item.task}
     </div>
   );
