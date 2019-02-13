6da5b84c9948962bb102a63ddc5c830d297b0a06
Joel Bartlett <joel@murbar.com>
2019-02-13 16:17:25 -0500

    Import TodoList

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import TodoList from './components/TodoComponents/TodoList';
 
 class App extends React.Component {
   constructor(props) {
@@ -10,6 +11,7 @@ class App extends React.Component {
     return (
       <div>
         <h1>Joel's Super Neat-o Todo App</h1>
+        <TodoList />
       </div>
     );
   }
