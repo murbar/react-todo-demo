28488b60fe61e686124923fc462d968cc99583e7
Joel Bartlett <joel@murbar.com>
2019-02-13 17:04:46 -0500

    Extract initial data

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,12 @@
 import React from 'react';
 import TodoList from './components/TodoComponents/TodoList';
+import getDummyData from './dummyData';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      items: [
-        {
-          task: 'Organize Garage',
-          id: 1528817077286,
-          completed: false
-        },
-        {
-          task: 'Bake Cookies',
-          id: 1528817084358,
-          completed: false
-        }
-      ]
+      items: [...getDummyData()]
     };
   }
 
---
+++ src/dummyData.js
@@ -0,0 +1,26 @@
+const getDummyData = () => {
+  return [
+    {
+      task: 'Organize garage',
+      id: 1528817077286,
+      completed: false
+    },
+    {
+      task: 'Call Mom',
+      id: 1528817077223,
+      completed: false
+    },
+    {
+      task: 'Plan vacation',
+      id: 1528817077255,
+      completed: true
+    },
+    {
+      task: 'Bake cookies',
+      id: 1528817084358,
+      completed: false
+    }
+  ];
+};
+
+export default getDummyData;
