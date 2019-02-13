9155887ec5ad5a123e6561cf50a85fe2799f2084
Joel Bartlett <joel@murbar.com>
2019-02-13 18:47:20 -0500

    Add clear handler

--- src/components/TodoComponents/TodoForm.jsx
+++ src/components/TodoComponents/TodoForm.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import './Todo.css';
 
-const TodoForm = ({ inputValue, changeInput, submitForm }) => {
+const TodoForm = ({ inputValue, changeInput, submitForm, clearCompleted }) => {
   return (
     <div className="todolist-form">
       <form onSubmit={submitForm}>
         <input type="text" placeholder="New todo" value={inputValue} onChange={changeInput} />
         <button type="submit">Add Todo</button>
-        <button type="button">Clear Completed</button>
+        <button type="button" onClick={clearCompleted}>
+          Clear Completed
+        </button>
       </form>
     </div>
   );
