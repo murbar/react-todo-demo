af57aab3b88a6fb4f8dc5b867847d06defd1303e
Joel Bartlett <joel@murbar.com>
2019-02-13 23:47:14 -0500

    Add task icons

--- src/components/TodoComponents/Todo.jsx
+++ src/components/TodoComponents/Todo.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './Todo.css';
 
 const Todo = ({ item, toggleCompleted }) => {
+  const iconSrc = item.completed ? '/icons/check-circle.svg' : '/icons/circle.svg';
   return (
     <li
       className="todolist-todo"
       onClick={() => toggleCompleted(item.id)}
       data-completed={item.completed}
     >
+      <img src={iconSrc} alt="Task icon" />
       {item.task}
     </li>
   );
