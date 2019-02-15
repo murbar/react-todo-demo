44adc44e70fcfd1bea5982603463a25fa78ca8d9
Joel Bartlett <6668097+murbar@users.noreply.github.com>
2019-02-15 18:36:39 -0500

    Hide component when completedCount is 0

--- src/components/ToggleHiddenButton.jsx
+++ src/components/ToggleHiddenButton.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 
-const ToggleHiddenButton = ({ showCompleted, onToggle }) => {
+const ToggleHiddenButton = ({ showCompleted, completedCount, onToggle }) => {
+  if (!completedCount) return null;
+
   const toolTip = `${showCompleted ? 'Hide' : 'Show'} completed tasks`;
   const imgSrc = showCompleted ? '/icons/eye-off.svg' : '/icons/eye.svg';
+
   return (
     <button className="control-button show-hide" type="button" title={toolTip} onClick={onToggle}>
       <img src={imgSrc} alt="Show/Hide icon" />
