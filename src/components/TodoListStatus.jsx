f07f70d33ea68506ff7592c4df1ea13ade7583a0
Joel Bartlett <6668097+murbar@users.noreply.github.com>
2019-02-15 16:14:50 -0500

    Rename var

--- src/components/TodoListStatus.jsx
+++ src/components/TodoListStatus.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const TodoListStatus = ({ showHidden, hiddenCount }) => {
   const taskPluralized = hiddenCount === 1 ? 'task' : 'tasks';
-  const shouldDisplayThis = hiddenCount > 0 && !showHidden;
+  const shouldDisplay = hiddenCount > 0 && !showHidden;
 
-  if (!shouldDisplayThis) return null;
+  if (!shouldDisplay) return null;
 
   return (
     <div className="hidden-todos-count">
