c27f30dec921b92168bad95b28290d68e770f5cd
Joel Bartlett <6668097+murbar@users.noreply.github.com>
2019-02-17 14:12:11 -0500

    Extract click handler

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -4,20 +4,20 @@ import './Todo.css';
 const Todo = ({ item, onToggleCompleted, onRemove }) => {
   const iconSrc = item.completed ? '/icons/check-circle.svg' : '/icons/circle.svg';
 
-  const onClick = e => {
+  const onClickTodo = () => {
+    onToggleCompleted(item.id);
+  };
+
+  const onClickRemoveButton = e => {
     e.stopPropagation();
     onRemove(item.id);
   };
 
   return (
-    <li
-      className="todolist-todo"
-      onClick={() => onToggleCompleted(item.id)}
-      data-completed={item.completed}
-    >
+    <li className="todolist-todo" onClick={onClickTodo} data-completed={item.completed}>
       <img src={iconSrc} alt="Task icon" />
       {item.task}
-      <button className="control-button" title="Remove task" onClick={onClick}>
+      <button className="control-button" title="Remove task" onClick={onClickRemoveButton}>
         <img src="/icons/delete.svg" alt="Delete icon" />
       </button>
     </li>
